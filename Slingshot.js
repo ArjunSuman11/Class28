d2b7df5ae38194a7e0e3217649f0f6f043f1b2bc
Arjun <anyway.arjun@gmail.com>
2020-12-06 12:59:50 +0530

    Slingshot effect

---
+++ Slingshot.js
@@ -0,0 +1,36 @@
+class Slingshot{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 10
+        }
+        this.sling = Constraint.create(options);
+        World.add(world, this.sling);
+    }
+
+    display(){
+        if(this.sling.bodyA){  //bodyA exists
+            var pointA = this.sling.bodyA.position;
+            var pointB = this.sling.pointB;
+            strokeWeight(4);
+            line(pointA.x, pointA.y, pointB.x, pointB.y);
+        }
+            
+    }
+    fly(){
+        //bodyA should be emptied
+        this.sling.bodyA = null;
+    }
+    
+    
+}
+/*
+draw(){
+    fly();
+    slingshot.fly();
+
+    null - nothing/empty
+}
+*/
